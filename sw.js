22a3a3b3033ab6b9e58f1707c70ae4399fdd13a3
wecanooo <wecanooo@gmail.com>
2018-12-18 01:00:39 +0900

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/story/app-7b5bc4002e313bc0e0db.js","98a16eb7620dd0eede4423456b28d054"],["/story/app-bbdddba20f1d184e8658.js","2028bc4c9537838664e59fdeac3c79ed"],["/story/commons-5384f82328964bce69ec.js","c8cf791603035ab065ba9a08543abbc9"],["/story/commons-65d2ded2abb45e3d9918.js","82f20cd5c51be02bc506594d93ab2506"],["/story/commons-8f308ff716ac9b48a2d0.js","22b64d287a9bf85f9ae72240db8eb8da"],["/story/commons-b0831dcd0ba0e4f5b389.js","646fd784580d092f1025ecf4b32fea1e"],["/story/index.html","1e169e9250df92ad949fe63a34f622d8"],["/story/offline-plugin-app-shell-fallback/index.html","c19c574d87f8a1ca20abec88352f6f29"]];
+var precacheConfig = [["/story/app-87baa7a502227b8ef073.js","a4e13328cecd1cde1263d735669ce0b3"],["/story/commons-d27227e2df29c58bc142.js","dedef27296834555c2e9a38d60b2ca29"],["/story/index.html","652bb71da7b2aa1cabea868e17a5fc48"],["/story/offline-plugin-app-shell-fallback/index.html","0f7c7924fe1c1c90d8f2f742e1f3c15b"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
