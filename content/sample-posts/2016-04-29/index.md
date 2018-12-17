631da2968c60a521954ea9833dbe75ed8dc64171
wecanooo <wecanooo@gmail.com>
2018-12-17 19:36:04 +0900

    add initial files

---
+++ content/sample-posts/2016-04-29/index.md
@@ -0,0 +1,37 @@
+---
+title: "Github와 Jekyll을 이용한 무료 블로그 만들기 - 1"
+cover: "images/cover7.jpg"
+author: "wecanooo"
+date: "2016-04-29"
+category: "tech"
+tags:
+    - blog
+    - jekyll
+    - github
+---
+
+![Jekyll and Github Pages](https://wecanooo.github.io/blog/assets/images/jekyll_github.png)
+
+---
+
+ 이번 에피소드에서는 [Github Pages](https://pages.github.com/)의 기능을 이용하여 간단한 블로그를 제작하는 방법을 알아보고자 한다. 많은 사람들이 블로그를 운영하기 위해서 [Wordpress](https://wordpress.org/)를 사용하고 있고, Wordpress는 상당히 훌륭한 CMS 툴임에는 분명하다. 이 글은 Wordpress가 나쁘고 Github Pages를 좋다고 얘기하고자 하는 것은 아님을 밝혀둔다.
+
+ Wordpress는 상당히 강력한 기능들과 더불어 많은 Plugin이 존재하여 많은 사람들이 사용하고 있지만 [Hosting 방식으로 Wordpress를 운영](https://wordpress.org/hosting/)할 것이 아니라면 모듈을 다운로드 받아 설치하는 방식으로 진행된다.
+
+ 그러다보니, Wordpress를 운영하기 위한 서버가 반드시 필요하게 되고 이는 금전적, 기술적 부담으로 다가오게 된다. 그래서, 이번 에피소드에서는 서버 필요없이, 모듈 설치 필요없이, 누구나 쉽게 자신만의 블로그를 운영할 수 있는 방법을 다뤄보고자 한다.
+
+### 이번 에피소드에서 다룰 것들
+
+* [Github Pages Generator를 이용하여 계정페이지 제작하기](https://wecanooo.github.io/blog/github-pages/)
+* [Jekyll 설치 및 Jekyll Theme 적용](https://wecanooo.github.io/blog/github-pages-with-theme/)
+* [Custom Domain 적용으로 나만의 URL 갖기](https://wecanooo.github.io/blog/github-pages-cname/)
+
+이것들이 이번 에피소드에서 다룰 주제이다. 위에 나열된 용어들이 다소 어렵게 느껴지더라도 미리 걱정하지 말자.
+차근차근 진행하다 보면 어렵지 않게 목표에 도달할 수 있을 것이다.
+
+### 미리 알려둘 사항
+
+* 이 에피소드에서 진행되는 모든 예제의 환경은 Mac 환경임을 알려둔다.
+* Github 계정은 생성했다고 가정한다.
+
+아무쪼록 이번 에피소드를 통해 많은 분들이 자신만의 블로그를 가질 수 있기를 바란다.
---
+++ data/SiteConfig.js
@@ -0,0 +1,52 @@
+module.exports = {
+  blogPostDir: "sample-posts", // The name of directory that contains your posts.
+  blogAuthorDir: "sample-authors", // The name of directory that contains your 'authors' folder.
+  blogAuthorId: "wecanooo", // The default and fallback author ID used for blog posts without a defined author.
+  siteTitle: "The way to find a dream", // Site title.
+  siteTitleAlt: "STUnitas 개발자의 블로그", // Alternative site title for SEO.
+  siteLogo: "/logos/dream.png", // Logo used for SEO and manifest. e.g. "/logos/logo-1024.png",
+  siteUrl: "https://wecanooo.github.io", // Domain of your website without pathPrefix.
+  pathPrefix: "/blog", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-casper/.
+  siteDescription: 'dreams.each { |dream| puts "a #{dream} is in my heart" }', // Website description used for RSS feeds/meta description tag.
+  siteCover: "https://haysclark.github.io/gatsby-starter-casper/images/blog-cover.jpg", // Optional, the cover image used in header for home page. e.g: "/images/blog-cover.jpg",
+  siteNavigation: true, // If navigation is enabled the Menu button will be visible
+  siteRss: "/rss.xml", // Path to the RSS file.
+  siteRssAuthor: "wecanooo", // The author name used in the RSS file
+  // siteFBAppID: "1825356251115265", // optional, sets the FB Application ID for using app insights
+  sitePaginationLimit: 10, // The max number of posts per page.
+  googleAnalyticsID: "102", // GA tracking ID.
+  // disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // enables Disqus comments, visually deviates from original Casper theme.
+  siteSocialUrls: [
+    "https://github.com/wecanooo",
+    "https://twitter.com/wecanooo",
+    "mailto:wecanooo@gmail.com"
+  ],
+  postDefaultCategoryID: "Tech", // Default category for posts.
+  // Links to social profiles/projects you want to display in the navigation bar.
+  userLinks: [
+    {
+      label: "GitHub",
+      url: "https://github.com/wecanooo",
+      iconClassName: "fa fa-github" // Disabled, see Navigation.jsx
+    },
+    {
+      label: "Twitter",
+      url: "https://twitter.com/wecanooo",
+      iconClassName: "fa fa-twitter" // Disabled, see Navigation.jsx
+    },
+    {
+      label: "Email",
+      url: "mailto:wecanooo@gmail.com",
+      iconClassName: "fa fa-envelope" // Disabled, see Navigation.jsx
+    }
+  ],
+  // Copyright string for the footer of the website and RSS feed.
+  copyright: {
+    label: "wecanooo@gmail.com", // Label used before the year
+    // year: "2018" // optional, set specific copyright year or range of years, defaults to current year
+    url: "https://www.about.me/wecanooo" // optional, set link address of copyright, defaults to site root
+  },
+  themeColor: "#c62828", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#e0e0e0", // Used for setting manifest background color.
+  promoteGatsby: true // Enables the GatsbyJS promotion information in footer.
+};
