ba5b2d0fe286a800c46e5a1e55b1d6f6e6a40f28
wecanooo <wecanooo@gmail.com>
2018-12-20 12:03:32 +0900

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/story/app-42a90d95d1c087e54805.js","e1fbb34f44f3082168e35d0afbfe2ae3"],["/story/commons-d27227e2df29c58bc142.js","6474e4cc067a0b8ce110a786df05a7cb"],["/story/index.html","d01dc2748adec948770c0350037bc30f"],["/story/offline-plugin-app-shell-fallback/index.html","07facd6e65cf939b135e58021b45c674"]];
+var precacheConfig = [["/story/app-42a90d95d1c087e54805.js","e1fbb34f44f3082168e35d0afbfe2ae3"],["/story/commons-d27227e2df29c58bc142.js","6474e4cc067a0b8ce110a786df05a7cb"],["/story/index.html","0e59d5e7223a088468b13fcd4050e376"],["/story/offline-plugin-app-shell-fallback/index.html","dea846736bf8166fe27fd3a30b3c284e"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
